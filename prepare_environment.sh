a6ee78cf02128dc6a9443baf8a190971cf0592c0
John Brännström <john.brannstrom@qamcom.se>
2018-02-09 15:30:37 +0100

    work in progress

--- prepare_environment.sh
+++ prepare_environment.sh
@@ -1,7 +1,18 @@
-#!/usr/bin/env bash
+#!/bin/bash -x
 
-# Tested on Ubuntu 16.04.3 LTS
-apt install ruby ruby-dev zlib1g-dev libffi-dev
+apt-get purge -y node nodejs npm
 
-gem install jekyll
+npm cache clean -f
+npm install -g n
+n stable
+apt remove -y nodejs
+ln -s /usr/local/bin/node /usr/bin/nodejs
+
+sudo apt-get -y update
+apt install -y ruby ruby-dev zlib1g-dev libffi-dev \
+               python-software-properties g++ make
+
 bundle update
+
+gem install jekyll -v 3.6.2
+gem uninstall jekyll -v '>3.6.2' --force
